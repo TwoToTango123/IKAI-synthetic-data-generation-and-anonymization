00a3c32063877ce24317f7e725b88cfe4b5819d7
wolfange <iskahamm007@gmail.com>
2026-03-24 13:32:15 -0500

    IKAI-25: Add reusable file upload component

--- frontend/src/pages/Anonymize.jsx
+++ frontend/src/pages/Anonymize.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
+import FileUpload from '../components/FileUpload'
 
 function Anonymize() {
   const [file, setFile] = useState(null)
+
+  const handleFileUpload = (uploadedFile) => {
+    setFile(uploadedFile)
+  }
 
   const handleAnonymize = () => {
     console.log('Anonymize clicked:', { fileName: file?.name || null })
@@ -11,19 +16,24 @@ function Anonymize() {
     <div className="container">
       <h1 className="section-title">Анонимизация данных</h1>
 
-      <div className="form-group">
-        <label className="form-label">Загрузите CSV файл:</label>
-        <input
-          type="file"
-          className="form-input"
-          accept=".csv"
-          onChange={(e) => setFile(e.target.files?.[0] || null)}
-        />
-      </div>
+      {!file && <FileUpload onFileUpload={handleFileUpload} />}
+
+      {file && (
+        <div className="file-info">
+          <span>Файл: {file.name}</span>
+          <button
+            className="btn btn-secondary btn-small"
+            onClick={() => setFile(null)}
+          >
+            Загрузить другой
+          </button>
+        </div>
+      )}
 
       <button
         className="btn btn-success btn-large btn-block"
         onClick={handleAnonymize}
+        disabled={!file}
       >
         Применить
       </button>
