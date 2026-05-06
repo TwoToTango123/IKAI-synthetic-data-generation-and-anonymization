79711c80ca8885a82f303879256ab6b8c4d638c5
wolfange <iskahamm007@gmail.com>
2026-05-06 02:38:52 -0500

    IKAI-71: align shared responsive shell with home layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import './App.css'
-import Header from './components/Header'
 import Frame from './pages/Home'
 import Generate from './pages/Generate'
 import Anonymize from './pages/Anonymize'
@@ -10,11 +9,10 @@ function App() {
 
   return (
     <div className="app">
-      {activeTab !== 'home' && <Header activeTab={activeTab} setActiveTab={setActiveTab} isHome={false} />}
       <main className="main-content">
         {activeTab === 'home' && <Frame setActiveTab={setActiveTab} />}
-        {activeTab === 'generate' && <Generate />}
-        {activeTab === 'anonymize' && <Anonymize />}
+        {activeTab === 'generate' && <Generate setActiveTab={setActiveTab} />}
+        {activeTab === 'anonymize' && <Anonymize setActiveTab={setActiveTab} />}
       </main>
     </div>
   )
